c474c23261a2d0cbc40717e66c968464f95c1de3
hemantsirsat <hemantsirsat40@gmail.com>
2020-10-03 11:22:48 +0530

    Singly Linked List

--- Linked List/python/single_linked_list.py
+++ Linked List/python/single_linked_list.py
@@ -1,7 +1,3 @@
-#Singly Linked List
-#Insert Beginning
-#Delete Beginning
-
 class Node:
     def __init__(self, value=None):
         self.value = value
@@ -11,45 +7,85 @@ class LinkedList:
     def __init__(self):
         self.headnode = None
         
-    def insert(self,new_data):
+    def insert_beg(self,new_data):
         new = Node(new_data)
-        new.next = self.headnode
+        if self.headnode == None:
+            self.headnode = new
+            new.nextnode = None
+            return
+        new.nextnode = self.headnode
         self.headnode = new
         
-    def delete(self):
+    def insert_end(self, new_data):
+        new = Node(new_data)
+        if self.headnode == None:
+            self.headnode = new
+            return
+        temp = self.headnode
+        while(temp.nextnode!=None):
+            temp = temp.nextnode
+        temp.nextnode = new
+        new.nextnode = None
+        
+    def delete_beg(self):
+        if self.headnode == None:
+            print("\nNo Elements Present\n")
+            return
         p = self.headnode
-        self.headnode = p.next
+        self.headnode = p.nextnode
+        
+    def delete_end(self):
+        if self.headnode == None:
+            print("\nNo Elements Present\n")
+            return
+        p = self.headnode
+        if p.nextnode == None:
+            self.headnode == None
+            return
+        while(p.nextnode.nextnode!=None):
+            p = p.nextnode
+        p.nextnode = None
     
     def printL(self):
+        if self.headnode == None:
+            print("\nNo Elements Present\n")
         temp = self.headnode
         while(temp):
             print(temp.value)
-            temp = temp.next
+            temp = temp.nextnode
             
-    def printrev(self, temp): 
-        if temp: 
-            self.printrev(temp.next) 
-            print(temp.value) 
-        else: 
+    def printrev(self, temp):
+        if self.headnode == None:
+            print("\nNo Elements Present\n")
             return
+        if temp: 
+            self.printrev(temp.nextnode) 
+            print(temp.value)
+            return
+        
             
     
 if __name__ == "__main__":
     llist = LinkedList()
     while(1):
         print("\nEnter the numbers: ")
-        print("\n1.Insert\t2.Delete\t3.Print\t4.Print Reverse\t5.Exit\n")
+        print("\n1.Insert Beginning\n2.Insert End\n3.Delete Beginning\n4.Delete End\n5.Print\n6.Print Reverse\n7.Exit\n")
         i = int(input())
         if i == 1:
             data = int(input())
-            llist.insert(data)
-        elif i == 2:
-            llist.delete()
+            llist.insert_beg(data)
+        elif i== 2:
+            data = int(input())
+            llist.insert_end(data)
         elif i == 3:
-            llist.printL()
+            llist.delete_beg()
         elif i == 4:
-            llist.printrev(llist.headnode)
+            llist.delete_end()
         elif i == 5:
+            llist.printL()
+        elif i == 6:
+            llist.printrev(llist.headnode)
+        elif i == 7:
             break
         else:
             print("\nEnter proper choice!\n")
